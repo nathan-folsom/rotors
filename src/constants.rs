e4f833a1f6a6cc6525ee87213aa67f30ca2f81ca
Nathan Folsom <n8folsom@gmail.com>
2023-10-10 21:21:43 -0700

    multiple iterations per frame

--- src/constants.rs
+++ src/constants.rs
@@ -1,11 +1,5 @@
 // Window Config
-pub const GRID_SIZE: usize = 200;
-pub const CELL_COUNT: usize = GRID_SIZE.pow(2);
-pub const CELL_WIDTH: f64 = WINDOW_WIDTH / GRID_SIZE as f64;
 pub const WINDOW_WIDTH: f64 = 1000.0;
 pub const WINDOW_HEIGHT: f64 = 1000.0;
 
-// Particle Config
-pub const PARTICLE_SIZE: f64 = 0.5;
-pub const PARTICLE_COUNT: i32 = 15_000;
-pub const CELL_INFLUENCE: f64 = 0.1;
+pub const ITERATIONS_PER_FRAME: i32 = 50_000;
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use crate::constants::*;
 use crate::math::*;
+use std::collections::VecDeque;
 use std::f64::consts::PI;
 use wasm_bindgen::prelude::*;
 use web_sys::CanvasRenderingContext2d;
@@ -30,6 +31,7 @@ pub struct FieldRenderer {
     rotors: (Rotor, Rotor),
     frame_count: i32,
     prev_point: Option<(f64, f64)>,
+    points: VecDeque<(f64, f64)>,
 }
 
 #[wasm_bindgen]
@@ -40,19 +42,20 @@ impl FieldRenderer {
             rotors: init_rotors(),
             frame_count: 0,
             prev_point: None,
+            points: VecDeque::new(),
         }
     }
 
     #[wasm_bindgen]
-    pub fn init(&self, ctx: &CanvasRenderingContext2d) {
+    pub fn init(&mut self, ctx: &CanvasRenderingContext2d) {
         self.render_background(ctx);
+        self.compute_points();
     }
 
     #[wasm_bindgen]
     pub fn render_frame(&mut self, ctx: &CanvasRenderingContext2d) -> i32 {
         self.frame_count += 1;
-        // self.render_background(ctx);
-        self.render_rotors(ctx);
+        self.render_line(ctx);
         self.frame_count
     }
 
@@ -60,40 +63,30 @@ impl FieldRenderer {
         ctx.set_fill_style(&JsValue::from_str("#03000f"));
         ctx.fill_rect(0.0, 0.0, WINDOW_WIDTH, WINDOW_HEIGHT);
         ctx.set_stroke_style(&JsValue::from("#ffffff"));
-
-        let render_rim = |rotor: &Rotor| {
-            ctx.begin_path();
-            let _ = ctx.ellipse(rotor.cx, rotor.cy, rotor.r, rotor.r, 0.0, 0.0, 2.0 * PI);
-            ctx.stroke();
-        };
-
-        // render_rim(&self.rotors.0);
-        // render_rim(&self.rotors.1);
     }
 
-    fn render_rotors(&mut self, ctx: &CanvasRenderingContext2d) {
-        ctx.set_stroke_style(&JsValue::from_str("#ff0000"));
-        let points = (self.rotors.0.get_point(), self.rotors.1.get_point());
-        let render_point = |point: (f64, f64)| {
-            let (x, y) = point;
-            ctx.begin_path();
-            let _ = ctx.ellipse(x, y, 2.0, 2.0, 0.0, 0.0, 2.0 * PI);
-            ctx.stroke();
-        };
-        self.rotors.0.advance();
-        self.rotors.1.advance();
-        // render_point(points.0);
-        // render_point(points.1);
-
-        let intersections = get_intersection(&self.rotors.0, &self.rotors.1);
-
-        if let Some((x, y)) = self.prev_point {
-            ctx.begin_path();
-            ctx.move_to(x, y);
-            ctx.line_to(intersections.1 .0, intersections.1 .1);
-            ctx.stroke();
+    fn compute_points(&mut self) {
+        for _ in 0..ITERATIONS_PER_FRAME {
+            let intersections = get_intersection(&self.rotors.0, &self.rotors.1);
+            self.points
+                .push_back((intersections.1 .0, intersections.1 .1));
+            self.rotors.0.advance();
+            self.rotors.1.advance();
         }
+    }
 
-        self.prev_point = Some((intersections.1 .0, intersections.1 .1));
+    fn render_line(&mut self, ctx: &CanvasRenderingContext2d) {
+        let (x, y) = self.points.pop_front().unwrap();
+        ctx.begin_path();
+        ctx.move_to(x, y);
+        let last = self.points.pop_back().unwrap();
+        while let Some((x, y)) = self.points.pop_front() {
+            ctx.line_to(x, y);
+        }
+        let (x, y) = last;
+        ctx.line_to(x, y);
+        ctx.stroke();
+        self.points.push_back(last);
+        self.compute_points();
     }
 }
